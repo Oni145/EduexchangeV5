f21661ce5992c4e74dc73f44bb6279350333d5d3
Oni <85605192+Oni145@users.noreply.github.com>
2024-04-28 08:45:47 +0800

    Update firebase.js

--- EduExchangeV3/js/firebase.js
+++ EduExchangeV3/js/firebase.js
@@ -1,14 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC9GI0X2Upei7oUrXGMIXk5dgcabJiYtZw",
-    authDomain: "eduexchange-29d22.firebaseapp.com",
-    databaseURL: "https://eduexchange-29d22-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "eduexchange-29d22",
-    storageBucket: "eduexchange-29d22.appspot.com",
-    messagingSenderId: "512860819362",
-    appId: "1:512860819362:web:d8f9b1f445d5a1f180acfb",
-    measurementId: "G-N08K17HEBS"
-  };
-
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
